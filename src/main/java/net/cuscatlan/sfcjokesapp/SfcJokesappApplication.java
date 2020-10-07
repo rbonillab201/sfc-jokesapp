3639082691a644bb334401d11b1b9ff0826e37c4
Renato Bonilla <renatio@gmail.com>
2020-10-07 14:15:20 -0600

    Commit Base

---
+++ src/main/java/net/cuscatlan/sfcjokesapp/SfcJokesappApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.sfcjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcJokesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfcJokesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cuscatlan/sfcjokesapp/SfcJokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.sfcjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfcJokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
